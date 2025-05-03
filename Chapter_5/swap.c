af7a5df9ad073344bd8645fe2c58923b57a59154
RUDRA PRASANNA MISHRA <rudrapmishra_en24b18_117@dtu.ac.in>
2025-05-03 18:47:49 +0000

    commit

--- Chapter_5/swap.c
+++ Chapter_5/swap.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
 
-
+void swap(int *a, int *b);
+void swap(int *a, int *b){
+    int temp;
+    temp = *a;
+    *a = *b;
+    *b = temp; 
+}
 int main() {
     int a= 4, b= 8;
-    swap(a,b);
+    swap(&a,&b);
+    printf("The value of a is %d and value of b is %d", a,b);
     return 0;
 }
