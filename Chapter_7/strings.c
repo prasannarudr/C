1b34706e4260d356b99184e1a9d61a0b25482609
RUDRA PRASANNA MISHRA <rudrapmishra_en24b18_117@dtu.ac.in>
2025-05-08 00:31:02 +0000

    commit

---
+++ Chapter_7/strings.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+  //  char st[]= {'a','b','c','\0'};
+  char st[]= "Rudra";
+    for(int i=0; i<5; i++)
+    {
+        printf("Character is %c \n", st[i]);
+    }
+    return 0;
+}
